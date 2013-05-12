8502358dd882dd01fd73a1bfc7d23889eb1239e0
Lee Edwards <leeredwards@gmail.com>
2013-05-11 22:03:29 -0400

    Make all template names snake case

--- client/templates/board.js
+++ client/templates/board.js
@@ -3,6 +3,6 @@ Template.board.rendered = function() {
   $("#reserve-deck").deckDraggable();
 };
 
-Template.board.tableCards = function() {
+Template.board.table_cards = function() {
   return Table.find();
 };
