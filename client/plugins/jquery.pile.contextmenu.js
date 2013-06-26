8f32e5b037d03c553652d605d9fd84f758db7185
Lee Edwards <leeredwards@gmail.com>
2013-06-26 11:04:43 -0400

    Extract Menu into its own file

--- client/plugins/jquery.pile.contextmenu.js
+++ client/plugins/jquery.pile.contextmenu.js
@@ -3,32 +3,7 @@
     $this = $(this);
 
     $this.qtip({
-      content: '\
-        <ul class="context-menu-items" data-context-menu="reserve-deck">\
-          <li><a href="#shuffle">Shuffle</a>\
-          <li><a href="#activate">Activate</a>\
-            <ul class="context-menu-subitems">\
-              <li><input maxlength="2" type="text" placeholder="1"/><label>Force</label></li>\
-            </ul>\
-          </li>\
-          <li><a href="#lose">Lose</a>\
-            <ul class="context-menu-subitems">\
-              <li><input maxlength="2" type="text" placeholder="1"/><label>Force</label></li>\
-            </ul>\
-          </li>\
-          <li><a href="#draw">Draw</a>\
-            <ul class="context-menu-subitems">\
-              <li><input maxlength="2" type="text" placeholder="1"/><label>cards</label></li>\
-            </ul>\
-          </li>\
-          <li><a href="#peek">Peek</a>\
-            <ul class="context-menu-subitems">\
-              <li><input maxlength="2" type="text" placeholder="1"/><label>cards</label></li>\
-            </ul>\
-          </li>\
-          <li><a href="#search">Search</a>\
-        </ul>\
-      ',
+      content: Asteroid.Menu.new(["activate", "lose", "draw", "peek", "shuffle", "search"], "reserve-deck"),
       position: { at: "top right", viewport: $("#board"), adjust: { x: 10, y: 2, method: "shift" } },
       style: { classes: "qtip-light context-menu", tip: false },
       show: { event: "contextmenu", solo: true },
